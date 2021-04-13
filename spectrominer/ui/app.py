fa2df7b4538fea46ded9bef9fc0e17f387a2ffa0
Gabriel Couture <gacou54@gmail.com>
2021-04-13 11:22:53 -0400

    Adding message popup

--- spectrominer/ui/app.py
+++ spectrominer/ui/app.py
@@ -1,35 +1,43 @@
 import tkinter
 from tkinter import filedialog, ttk
+from typing import Optional
+
+from spectrominer.parser import Parser
+from spectrominer.ui.popup import PopUp
 
 
 class App(tkinter.Frame):
 
     def __init__(self, root: tkinter.Tk):
+        self.parser: Optional[Parser] = None
         super().__init__(root)
         self.root = root
 
-        self.winfo_toplevel().title('Spectrominer')  # Title
-
-        self.inline_filepath = tkinter.Entry(self.root)
-        self.btn_browse = tkinter.Button(self.root, text='Browse', command=self.browse_file)
-
-        self.inline_filepath.place(x=20, y=20, width=600, height=30)
-        self.btn_browse.place(x=640, y=20, width=140, height=30)
+        ttk.Style(self.root).theme_use('clam')
 
         self.root.geometry('800x600')
+        self.winfo_toplevel().title('Spectrominer')  # Title
+
+        self.label_browse_file = ttk.Label(self.root, text='Select a file')
+        self.inline_filepath = ttk.Entry(self.root)
+        self.btn_browse = ttk.Button(self.root, text='Browse', command=self.browse_file)
+
+        self.label_browse_file.place(x=20, y=20)
+        self.inline_filepath.place(x=20, y=50, width=600, height=30)
+        self.btn_browse.place(x=640, y=50, width=140, height=30)
 
     def browse_file(self):
         filepath = filedialog.askopenfilename(title='Browse')
-        self.inline_filepath.delete(0, tkinter.END)
-        self.inline_filepath.insert(0, filepath)
-        print(filepath)
+        try:
+            self.parser = Parser(filepath)
+            self.inline_filepath.delete(0, tkinter.END)
+            self.inline_filepath.insert(0, filepath)
+        except Exception as e:
+            PopUp(self.root, message=str(e))
 
 
 if __name__ == '__main__':
     root = tkinter.Tk()
     app = App(root)
-
-    style = ttk.Style(root)
-    style.theme_use('clam')
 
     app.mainloop()
---
+++ spectrominer/ui/popup.py
@@ -0,0 +1,17 @@
+import tkinter
+from tkinter import ttk
+
+
+class PopUp(tkinter.Toplevel):
+
+    def __init__(self, root: tkinter.Tk, message: str):
+        super(PopUp, self).__init__(root)
+        self.geometry('400x300')
+
+        self.wm_title('Message')
+
+        label = ttk.Label(self, text=message, anchor=tkinter.N)
+        label.place(x=20, y=10, width=360, height=230)
+
+        btn = ttk.Button(self, text='Ok', command=self.destroy)
+        btn.place(x=320, y=250, width=60, height=30)
